dc2c49897e53e29ba377818ccd2d1d7525798199
AlphaMastar <101194190+AlphaMastar@users.noreply.github.com>
2022-10-17 14:40:19 +0000

    完善，bugfix Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/4a17b156.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/9daba997.html","posts/980297d6.html","posts/4a17b156.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
