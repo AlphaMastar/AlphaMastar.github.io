ecefe1b08e5a2b0fa49a64f0b7e2ae841da189b7
AlphaMastar <101194190+AlphaMastar@users.noreply.github.com>
2023-04-22 07:18:06 +0000

    音乐页面修改、每日一言、图标修复 Updated By Github Actions

---
+++ js/poetry.js
@@ -0,0 +1,9 @@
+// window.addEventListener("load", () => {
+//     // 每日一言
+//     getScript('https://sdk.jinrishici.com/v2/browser/jinrishici.js').then(() => {
+//       jinrishici.load(result =>{
+//         document.getElementsByClassName('todayCard-title').item(0).innerHTML = result.data.content
+//       })
+//     })
+// })
+//- https://v1.jinrishici.com/all.json
--- js/utils.js
+++ js/utils.js
@@ -742,7 +742,7 @@ const anzhiyu = {
     }
     console.info(window.location.pathname);
     const urlParams = new URLSearchParams(window.location.search);
-    const userId = "8152976493";
+    const userId = "7617692888";
     const userServer = "netease";
     const anMusicPageMeting = document.getElementById("anMusic-page-meting");
     if (urlParams.get("id") && urlParams.get("server")) {
