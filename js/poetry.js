60e3e4685789118c6a769d6edc4bc4b1c395d564
AlphaMastar <101194190+AlphaMastar@users.noreply.github.com>
2023-04-27 10:30:48 +0000

    一些小修改 Updated By Github Actions

--- js/poetry.js
+++ js/poetry.js
@@ -1,4 +1,4 @@
-// https://v1.jinrishici.com/all.json
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script>// https://v1.jinrishici.com/all.json
 !function(e) {
     var n, t = {}, o = "jinrishici-token";
     function i() {
