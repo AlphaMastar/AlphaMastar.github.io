f32f5ad2d8ee1cc2e064f256f81fbaeb411e3f10
AlphaMastar <101194190+AlphaMastar@users.noreply.github.com>
2023-05-13 01:09:29 +0000

    主题版本更新至1.2.0 Updated By Github Actions

--- js/modify.js
+++ js/modify.js
@@ -1,9 +1,9 @@
 !function modify() {
-    let condition = document.getElementsByClassName("is-center-tag").item(0);
+    let condition = document.getElementsByClassName("is-center").item(0);
     if (condition != null){
         page.style.background='none';
         page.style.boxShadow='none';
-        page.style.padding='0';
         page.style.border='none';
+        tag.style.background='none';
     }
 }(window);
