71ffa38c06f7bd3d9c3127cddc2b9146d23e8486
AlphaMastar <101194190+AlphaMastar@users.noreply.github.com>
2023-05-20 07:32:29 +0000

    新增页面，bugfix，新文章 Updated By Github Actions

--- js/modify.js
+++ js/modify.js
@@ -1,6 +1,8 @@
 !function modify() {
-    let condition = document.getElementsByClassName("is-center").item(0);
-    if (condition != null){
+    let condition_1 = document.getElementsByClassName("is-center").item(0);
+    let condition_2 = document.getElementsByClassName("page-title").item(0);
+    let condition_3 = document.getElementById("tag");
+    if (condition_1 != null, condition_2 != null, condition_3 != null) {
         page.style.background='none';
         page.style.boxShadow='none';
         page.style.border='none';
