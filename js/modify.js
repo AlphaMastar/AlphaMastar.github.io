d9c54948c648dfb7291a9491a8c976e6c833e256
AlphaMastar <101194190+AlphaMastar@users.noreply.github.com>
2023-05-28 13:15:57 +0000

    tag_js性能提高 Updated By Github Actions

--- js/modify.js
+++ js/modify.js
@@ -1,11 +1,8 @@
 !function modify() {
-    let condition_1 = document.getElementsByClassName("is-center").item(0);
-    let condition_2 = document.getElementsByClassName("page-title").item(0);
-    let condition_3 = document.getElementById("tag");
-    if (condition_1 != null, condition_2 != null, condition_3 != null) {
+    if (location.pathname == '/tags/') {
         page.style.background='none';
         page.style.boxShadow='none';
         page.style.border='none';
         tag.style.background='none';
-    };
+    }
 }(window);
