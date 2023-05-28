424405868ab32515e30d44a52329eeb48c7b4760
AlphaMastar <101194190+AlphaMastar@users.noreply.github.com>
2023-05-28 12:46:18 +0000

    上传所有源码，修复友链页面 Updated By Github Actions

--- js/modify.js
+++ js/modify.js
@@ -2,17 +2,10 @@
     let condition_1 = document.getElementsByClassName("is-center").item(0);
     let condition_2 = document.getElementsByClassName("page-title").item(0);
     let condition_3 = document.getElementById("tag");
-    let condition_4 = document.getElementById("flink-banners")
     if (condition_1 != null, condition_2 != null, condition_3 != null) {
         page.style.background='none';
         page.style.boxShadow='none';
         page.style.border='none';
         tag.style.background='none';
     };
-    if (condition_4 != null) {
-        var twikooSubmit = document.getElementsByClassName("tk-submit")[0];
-        if (twikooSubmit) {
-        twikooSubmit.style.opacity = "0";
-        }
-    }
 }(window);
