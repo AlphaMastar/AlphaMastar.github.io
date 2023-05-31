abaddabd5bd863f848a63766ec2b4c8d932134bb
AlphaMastar <101194190+AlphaMastar@users.noreply.github.com>
2023-05-31 01:43:33 +0000

    js性能提高 Updated By Github Actions

--- js/modify.js
+++ js/modify.js
@@ -1,8 +1,19 @@
 !function modify() {
-    if (location.pathname == '/tags/') {
-        page.style.background='none';
-        page.style.boxShadow='none';
-        page.style.border='none';
-        tag.style.background='none';
-    }
+    (function tagPagefix() {
+        if (location.pathname == '/tags/') {
+            page.style.background='none';
+            page.style.boxShadow='none';
+            page.style.border='none';
+            tag.style.background='none';
+        }
+    }());
+    (function commentFix() {
+        var value = document.getElementById('twikoo');
+        if (value && location.pathname != '/link/') {
+            document.getElementsByClassName('el-input__count')[0].innerHTML = '0/500';
+            var input = document.getElementsByClassName('el-textarea__inner')[0];
+            input.value = '';
+            input.style.height = '121px';
+        }
+    }());
 }(window);
