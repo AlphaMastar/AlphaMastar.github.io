2de96115d398cf8fd21a4a3d5daf5b28d1db70c4
AlphaMastar <101194190+AlphaMastar@users.noreply.github.com>
2025-06-06 14:23:14 +0000

    修改控制台打印滑动长度 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/2c23d359.html","posts/9daba997.html","posts/980297d6.html","posts/74545c96.html","posts/4a17b156.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/2c23d359.html","posts/980297d6.html","posts/9daba997.html","posts/74545c96.html","posts/4a17b156.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
